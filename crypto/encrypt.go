a234cc8cd944a88dca63a7636d05f99856e5796b
carles.calsina <carles.calsina@atrapalo.com>
2019-06-12 18:22:52 +0200

    move crypto functions to package cryto

--- crypto/encrypt.go
+++ crypto/encrypt.go
@@ -1,4 +1,4 @@
-package main
+package crypto
 
 const clau = "FRIENDSOFGO"
 
--- main.go
+++ main.go
@@ -1,11 +1,14 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"github.com/xespona/goworkshops/crypto"
+)
 
 func main() {
 	a := "FOR A MOMENT, NOTHING HAPPENED. THEN, AFTER A SECOND OR SO, NOTHING CONTINUED TO HAPPEN"
-	s := Encrypt(a)
+	s := crypto.Encrypt(a)
 
 	fmt.Println(s)
-	fmt.Println(Decrypt(s))
+	fmt.Println(crypto.Decrypt(s))
 }
